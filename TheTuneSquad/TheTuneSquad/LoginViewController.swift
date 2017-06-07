a7336b65585a946ce2253515c2c4272f4ea86d3d
cleex1004 <cleex1004@gmail.com>
2017-06-07 13:29:35 -0700

    created model objects

---
+++ TheTuneSquad/TheTuneSquad/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  TheTuneSquad
+//
+//  Created by Christina Lee on 6/7/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ TheTuneSquad/TheTuneSquad/PlaylistViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaylistViewController.swift
+//  TheTuneSquad
+//
+//  Created by Christina Lee on 6/7/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
